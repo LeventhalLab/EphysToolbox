8ee17c88d22b79e0a29c378f5a2edbbcbecec220
Matt Gaidica <matt@gaidi.ca>
2015-05-21 21:44:44 -0400

    Use butter filt

--- TDTScripts/ezDDT.m
+++ TDTScripts/ezDDT.m
@@ -1,7 +1,8 @@
 function sev=ezDDT()
+[b,a] = butter(4, [0.02 0.2]);
 
 [f,p]=uigetfile({'*.sev'},'Select File...','~/Documents/Data');
 [sev,header] = read_tdt_sev(fullfile(p,f));
-sev = wavefilter(sev,6);
+sev = filtfilt(b,a,double(sev));
 sev = artifactThresh(sev,1,600);
 ddt_write_v(fullfile(p,[f,'.ddt']),1,length(sev),header.Fs,sev/1000);
