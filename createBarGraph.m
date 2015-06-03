4c7c35adba530493dbd8f87eede68d0fb49dc3d5
soiferh <soiferh@umich.edu>
2015-06-03 13:04:29 -0400

    Fixed things

--- createBarGraph.m
+++ createBarGraph.m
@@ -3,19 +3,20 @@ function createHistogram(filename)
 
 [nvar, names, types] = nex_info(filename);
 for i = 1:nvar
-    varname = names(i);
+    %not sure if this will work if there is more than one name?
+    varname = names;
     [n, ts] = nex_ts(filename, varname);
     firingRate = [];
+    a = 1;
     for ii = 1:30:3600
         numSpikes = 0;
-        a = 1;
-        while ts(ii) < a+30
+        while ts(a) < ii+30
             numSpikes = numSpikes + 1;
             a = a+1;
         end
         firingRate = [firingRate numSpikes/30];
     end
-    t = linspace(0:60:61);
+    t = linspace(0,60,120);
     figure
     bar(t, firingRate, 'hist')
     xlabel('time (min)');
