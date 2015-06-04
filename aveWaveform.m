54cf6268ed6c2135cbf745d47bd1f9b7176dc314
soiferh <soiferh@umich.edu>
2015-06-04 16:36:28 -0400

    comments

--- aveWaveform.m
+++ aveWaveform.m
@@ -1,20 +1,40 @@
 function aveWaveform(ts, SEVfilename)
+%Function to plot the average waveform of a unit with the peak centered at
+%zero. Default color of summer tree green. Default window size of 4 ms.
+%
+%Inputs: 
+%   ts - vector of time stamps
+%   SEVfilename - the path for the SEV file of interest
+% 
+% possible variable inputs: color, window size
+
 summerTreeColor = [145/255, 205/255, 114/255];
+
+%Read in data and filter
 [sev, header] = read_tdt_sev(SEVfilename);
 [b,a] = butter(4, [.02, .2]);
 for ii = 1:size(sev,1)
     sev(ii,:) = filtfilt(b,a,double(sev(ii,:)));
 end
+
 windowSize = round(.002*header.Fs);
 waveforms = [];
+
+%Create the segments of the wave form that are 2ms on either side of the
+%peak
 for ii = 1:length(ts)
     waveforms = [waveforms; sev(round(header.Fs*ts(ii))-windowSize:round(header.Fs*ts(ii))+windowSize)]     
 end    
 
+%Calculate the mean for each column in the waveform vector
 meanWave = mean(waveforms,1);
+
+%Calculate the standard deviations
 stdDev = std(waveforms);
 upperStd = meanWave + stdDev;
 lowerStd = meanWave - stdDev;
+
+%Plot the waveform and shade upper and lower standard deviations
 figure
 t = linspace(-2, 2, length(meanWave));
 fill([t fliplr(t)], [upperStd fliplr(lowerStd)], summerTreeColor, 'edgeColor', summerTreeColor);
--- createBarGraph.m
+++ createBarGraph.m
@@ -1,13 +1,23 @@
 function createBarGraph(filename)
 %Create a histogram for the hour long session with bin size of 30 seconds
+% Inputs:
+%   filename -  NEX file path
+%
+%
+% possible variable inputs: length of session, bin size
 
+%Get the timestamps for each channel
 tsCell = leventhalNexTs(filename);
 for ii = 1:size(tsCell,1)
     firingRate = [];
     for x = 1:30:3600
+        %Find the number of spikes in the 30 sec range and find the firing
+        %rate
         numSpikes = length(find(tsCell{ii,2} < x+30 & tsCell{ii,2}>=x));
         firingRate = [firingRate numSpikes/30];
     end
+    
+    %plot in the histogram format
     t = linspace(0,60,120);
     figure
     bar(t, firingRate, 'hist')
