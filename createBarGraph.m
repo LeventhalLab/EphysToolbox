4d4aa5269940f6a5955ff9289174e37bfeea1174
soiferh <soiferh@umich.edu>
2015-06-04 12:09:54 -0400

    Changed to use leventhalNexTs function

--- createBarGraph.m
+++ createBarGraph.m
@@ -1,19 +1,11 @@
-function createHistogram(filename)
+function createBarGraph(filename)
 %Create a histogram for the hour long session with bin size of 30 seconds
 
-[nvar, names, types] = nex_info(filename);
-for i = 1:nvar
-    %not sure if this will work if there is more than one name?
-    varname = names;
-    [n, ts] = nex_ts(filename, varname);
+tsCell = leventhalNexTs(filename);
+for ii = 1:size(tsCell,1)
     firingRate = [];
-    a = 1;
-    for ii = 1:30:3600
-        numSpikes = 0;
-        while ts(a) < ii+30
-            numSpikes = numSpikes + 1;
-            a = a+1;
-        end
+    for t = 1:30:3600
+        numSpikes = length(find(tsCell{ii,2} < t+30));
         firingRate = [firingRate numSpikes/30];
     end
     t = linspace(0,60,120);
