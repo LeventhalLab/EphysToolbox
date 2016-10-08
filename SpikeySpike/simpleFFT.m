bf120a1cf2db2539b1a48713b495d8533b6b6dc7
Matt Gaidica <matt@gaidi.ca>
2016-10-07 22:00:11 -0400

    Extending function

--- SpikeySpike/simpleFFT.m
+++ SpikeySpike/simpleFFT.m
@@ -1,4 +1,8 @@
-function simpleFFT(data,Fs)
+function [A,f] = simpleFFT(varargin)
+% simpleFFT(data,Fs,newFigure);
+
+data = varargin{1};
+Fs = varargin{2};
 T = 1/Fs; % Sample time
 L = length(data); % Length of signal
 t = (0:L-1)*T; % Time vector
@@ -8,8 +12,14 @@ f = Fs/2*linspace(0,1,NFFT/2+1);
 Y = fft(double(data),NFFT)/L;
 A = 2*abs(Y(1:NFFT/2+1));
 
-figure;
-plot(f,A);
-xlim([5 500]);
-xlabel('Frequency (Hz)')
-ylabel('|Y(f)|')
+if length(varargin) > 2
+    if varargin{3}
+        figure;
+    else
+        hold on;
+    end
+    semilogy(f,smooth(A,round(Fs/1000)));
+    xlim([10 100]);
+    xlabel('Frequency (Hz)')
+    ylabel('|Y(f)|')
+end
