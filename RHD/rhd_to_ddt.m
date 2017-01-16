f954b616e9898fb375ba2f6906d276ce110ed6ff
carswift <carswift@umich.edu>
2017-01-16 13:42:08 -0500

    Create rhd_to_ddt.m

---
+++ RHD/rhd_to_ddt.m
@@ -0,0 +1,14 @@
+read_Intan_RHD2000_file;
+
+%create filter
+[b,a] = butter(4, [0.02 0.2]);
+
+%filter all channels
+f_amplifier_data = zeros(length(amplifier_channels), length(t_amplifier));
+for channel = 1:length(amplifier_channels)
+    f_amplifier_data(channel,:) = filtfilt(b,a,amplifier_data(channel,:));
+end
+
+%write individual channels to .ddt (replace X with channel number)
+%ddt_write_v('channel_X.ddt',1,length(f_amplifier_data(X,:)),frequency_parameters.amplifier_sample_rate,f_amplifier_data(X,:)/1000)
+
